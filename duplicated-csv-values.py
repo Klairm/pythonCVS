a6688b8b67c9673165b364f6644ec80c6a24fe09
Klairm <ivangardis@hotmail.com>
2019-10-16 17:55:28 +0200

    English language

---
+++ duplicated-csv-values.py
@@ -0,0 +1,26 @@
+import csv,collections,sys
+
+try:
+    file = sys.argv[1]
+    argc= len(sys.argv)
+    lista = list()
+    if argc==2:
+        csvfile = open(file,encoding="utf-8") 
+
+        spamreader = csv.reader(csvfile,delimiter="," )
+        
+        for row in spamreader:
+            lista.append(row[1])
+        pass
+    else:
+        print("Usage: python3 valores_duplicados_csv <file.csv>")
+        pass
+    print("\n Duplicated values: ")
+    print([item for item, count in collections.Counter(lista).items() if count > 1])
+except FileNotFoundError:
+    print("File not found.")
+except IndexError:
+    print("Usage: python3 valores_duplicados_csv <file.csv>")
+
+
+
