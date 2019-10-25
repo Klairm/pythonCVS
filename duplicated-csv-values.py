3fe533219d069d381fc54040e50a4754782bc4ed
Klairm <ivangardis@hotmail.com>
2019-10-25 03:40:59 +0200

    Clean code

--- duplicated-csv-values.py
+++ duplicated-csv-values.py
@@ -1,26 +1,32 @@
 import csv,collections,sys
 
-try:
-    file = sys.argv[1]
-    argc= len(sys.argv)
-    lista = list()
-    if argc==2:
-        csvfile = open(file,encoding="utf-8") 
+def csv_check():
+    try:
+        file = sys.argv[1]
+        argc= len(sys.argv)
+        lista = list()
+        if argc==2:
+            csvfile = open(file,encoding="utf-8") 
 
-        spamreader = csv.reader(csvfile,delimiter="," )
+            spamreader = csv.reader(csvfile,delimiter="," )
         
-        for row in spamreader:
-            lista.append(row[1])
-        pass
-    else:
+            for row in spamreader:
+                lista.append(row[1])
+                pass
+        else:
+            print("Usage: python3 duplicated-csv-values.py <file.csv>")
+            pass
+        print("\n Duplicated values: ")
+        print([item for item, count in collections.Counter(lista).items() if count > 1])
+        
+    except FileNotFoundError:
+        print("File not found.")
+    except IndexError:
         print("Usage: python3 duplicated-csv-values.py <file.csv>")
-        pass
-    print("\n Duplicated values: ")
-    print([item for item, count in collections.Counter(lista).items() if count > 1])
-except FileNotFoundError:
-    print("File not found.")
-except IndexError:
-    print("Usage: python3 duplicated-csv-values.py <file.csv>")
+
+
+csv_check()
+
 
 
 
